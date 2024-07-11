70fb5cb3e2c0e1f295ddcb6c223717c49795b0c2
Kinga Miszczak <kinga.miszczak@simpleformat.pl>
2024-07-11 09:41:41 +0200

    configuration

--- azure-app-km-vs/Pages/Index.cshtml.cs
+++ azure-app-km-vs/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace azure_app_km_vs.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _confifuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _confifuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _confifuration["Greeting"];
         }
     }
 }
